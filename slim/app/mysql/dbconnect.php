1ef9d15b64a03246f615b6751a38836cd75d1879
Karl Krabbenhoft <karlkrabbenhoft@gmail.com>
2020-10-31 09:09:52 +0200

    Initial Commit

---
+++ slim/app/mysql/dbconnect.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $pass = "root";
+    $db_name = "TeleOppsDB";
+    $mysqli = new mysqli($host, $user, $pass, $db_name);
+?>
---
+++ slim/app/routes.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {    
+
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
+    $app->get('/hello/{name}', function(Request $request, Response $response, $args) {
+        $name = $args['name'];
+        $response->getBody()->write( "Hello, $name");
+        return $response;
+    });
+
+    $app->post('/api/insertuser', function(Request $request, Response $response) {
+        require_once(__DIR__.'/mysql/dbconnect.php');
+        $body = $request->getBody();
+        $bodyArray = json_decode($body, true);
+        $username = $bodyArray['username'];
+        $response->getBody()->write("Hello, $username");
+        return $response;
+        // $query = "INSERT INTO  TeleUsers SET user";
+    });
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+};
