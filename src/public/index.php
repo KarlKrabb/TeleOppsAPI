74a97e65536c5fd9fbade8f2a1071361ef87e867
Karl Krabbenhoft <karlkrabbenhoft@gmail.com>
2020-10-31 21:20:24 +0200

    Cors setup

--- src/public/index.php
+++ src/public/index.php
@@ -1,10 +1,29 @@
 <?php
-
 require '../vendor/autoload.php';
 
+use Slim\Exception\HttpNotFoundException;
 $app = new \Slim\App();
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
+
 include 'routes/RouteManager.php';
+
+// Catch-all route to serve a 404 Not Found page if none of the routes match
+// NOTE: make sure this route is defined last
+$app->map(['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], '/{routes:.+}', function($req, $res) {
+    $handler = $this->notFoundHandler; // handle using the default Slim page not found handler
+    return $handler($req, $res);
+});
 
 $app->run();
 
